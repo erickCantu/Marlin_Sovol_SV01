e2e61fbfd3e7e729b53c7b26ce263595ceed8fd0
Erick Cantu Perez <ecantu@tuta.io>
2025-03-13 12:43:35 +0100

    G34. Z-Leveling. Increase the iterations to 10 as more than 5 are needed to achive accuracy after power on. Reduce the accuracy to 0.01. It increases printing stabilty and the machine can achieve it.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1020,8 +1020,8 @@
 
   // On a 300mm bed a 5% grade would give a misalignment of ~1.5cm
   #define G34_MAX_GRADE              5    // (%) Maximum incline that G34 will handle
-  #define Z_STEPPER_ALIGN_ITERATIONS 5    // Number of iterations to apply during alignment
-  #define Z_STEPPER_ALIGN_ACC        0.02 // Stop iterating early if the accuracy is better than this
+  #define Z_STEPPER_ALIGN_ITERATIONS 10    // Number of iterations to apply during alignment //ECP Add More iterations as it is needed in this machine. 
+  #define Z_STEPPER_ALIGN_ACC        0.01 // Stop iterating early if the accuracy is better than this //ECP Tight the accuracy, it is possible to achieve it. Provides printing stability.
   #define RESTORE_LEVELING_AFTER_G34      // Restore leveling after G34 is done?
   // After G34, re-home Z (G28 Z) or just calculate it from the last probe heights?
   // Re-homing might be more precise in reproducing the actual 'G28 Z' homing height, especially on an uneven bed.
